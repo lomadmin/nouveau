f09d08526ef455965eb04fa7a9bde30c12d11ee5
Mamadou <lommamadoulom@gmail.com>
2023-03-08 15:22:34 +0100

    Modification des pages

--- app/index.php
+++ app/index.php
@@ -12,5 +12,16 @@
                 le boss of the bosses , the gladiator of gladiators
                 I am a man 😁
         </h1>
+        <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt obcaecati, voluptatem reiciendis porro ut mollitia repellendus minus vel quaerat qui tempora ipsam corporis rem animi quae quo distinctio. A, fugit.
+        Sequi animi quam adipisci ut magni, temporibus quidem neque blanditiis iure, sit accusamus, sapiente nemo. Ad saepe eveniet non odit expedita? Saepe aperiam rem eveniet in corporis cum consequuntur aspernatur.
+        Quidem dolorum neque et repellat. Minima cum alias non, eaque quibusdam, similique natus, quidem blanditiis distinctio autem molestias suscipit. Quaerat, suscipit? Consequatur fuga asperiores, debitis ratione amet laborum aut tempora?
+        Fugiat nulla perferendis sed corrupti praesentium numquam esse cumque facere cum id itaque, rerum impedit placeat, architecto, tempore maxime sit! Reiciendis sed, asperiores voluptates eius omnis numquam porro distinctio fuga.
+        Molestiae error, quis fugiat sapiente sunt reprehenderit dignissimos fugit velit eligendi asperiores id laudantium temporibus culpa quod. Sunt architecto accusantium esse itaque tempore tempora in numquam magnam fugiat, ut quidem?
+        Odio similique recusandae aliquid? Voluptatum laboriosam quas nostrum cumque quibusdam, tempora facere odio nisi fugiat omnis dignissimos, aspernatur laborum sit? Dolore, similique nihil voluptas qui unde impedit velit recusandae molestias?
+        Officiis quo maiores qui quia expedita molestiae, odit nesciunt placeat tempore vero repellendus mollitia rem aut sed nisi eos asperiores odio. Inventore dolorum dolorem ducimus, tenetur consectetur aut cupiditate ut.
+        In voluptatibus minima molestiae quos corporis. Voluptas commodi esse amet blanditiis obcaecaero assumenda dolorem molestiae impedit iusto blanditiis doloremque quaerat est porro fuga illum non dolore iste! Rem in
+         repudiandae suscipit.
+        </div>
     </body>
 </html>
