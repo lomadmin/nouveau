edefa683f0f67e213bbc2ada85e93ea17a4332ae
Mamadou <lommamadoulom@gmail.com>
2023-03-08 14:18:56 +0100

    Version initiale

---
+++ app/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            welcome to my area, waring because my panther is very dangerous
+        </title>
+        <link rel="stylesheet" href="CSS/index.css">
+    </head>
+    <body>
+        <h1>
+            je suis Mamadou Lom  </br>
+                le boss of the bosses , the gladiator of gladiators
+                I am a man 😁
+        </h1>
+    </body>
+</html>
